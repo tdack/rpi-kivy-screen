ac2fc9b6b219bee3d859608377681d076eaaf012
Troy Dack <troy@dack.com.au>
2016-01-13 23:48:18 +1100

    Rewrite to use .kv for layout
    Widgets are placed using definition in .kv file.  Custom properties are
    used to assign GPIO resources to specific widgets so each widget can
    control the GPIO.

--- main.py
+++ main.py
@@ -1,113 +1,129 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.9.0')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.image import Image
 from kivy.uix.slider import Slider
 from kivy.clock import Clock
-from kivy.graphics import Color, Rectangle
+from kivy.uix.togglebutton import ToggleButton
+from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty
 
 import RPi.GPIO as GPIO
 
-#for now, use a global for blink speed (better implementation TBD):
-speed = 1.0
+""" Base class for a GPIO based button """
+class GPIOButton(Button):
+	pin = NumericProperty(None)
+	direction = NumericProperty(None)
+	pull_up_down = NumericProperty(None, allownone=True)
+	gpio_value = ObjectProperty(None)
+	gpio_configured = BooleanProperty(False)
 
-# Set up GPIO:
-beepPin = 17
-ledPin = 27
-buttonPin = 22
-flashLedPin = 10
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(beepPin, GPIO.OUT)
-GPIO.output(beepPin, GPIO.LOW)
-GPIO.setup(ledPin, GPIO.OUT)
-GPIO.output(ledPin, GPIO.LOW)
-GPIO.setup(flashLedPin, GPIO.OUT)
-GPIO.output(flashLedPin, GPIO.LOW)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	def on_press(self):
+		# work around widget properties not being set
+		# when __init__() is called
+		if not self.gpio_configured:
+			if GPIO.getmode() is None:
+				GPIO.setmode(GPIO.BOARD)
+			print("setting up pin {}".format(self.pin))
+			if self.pull_up_down is not None:
+				GPIO.setup(self.pin, self.direction,
+					pull_up_down=self.pull_up_down)
+			else:
+				GPIO.setup(self.pin, self.direction)
+			if self.gpio_value is not None:
+				GPIO.output(self.pin, self.gpio_value)
+			self.gpio_configured = True
+		return False
 
-# Define some helper functions:
+""" A button that changes state based on input from a GPIO pin """
+class GPIOInputButton(GPIOButton):
+	def __init__(self, **kwargs):
+		super(GPIOInputButton, self).__init__(**kwargs)
+		# Set a timer to check if the GPIO pin has changed state
+		Clock.schedule_interval(self.update, 1.0/10.0)
 
-# This callback will be bound to the LED toggle and Beep button:
-def press_callback(obj):
-	print("Button pressed,", obj.text)
-	if obj.text == 'BEEP!':
-		# turn on the beeper:
-		GPIO.output(beepPin, GPIO.HIGH)
-		# schedule it to turn off:
-		Clock.schedule_once(buzzer_off, .1)
-	if obj.text == 'LED':
-		if obj.state == "down":
-			print ("button on")
-			GPIO.output(ledPin, GPIO.HIGH)
-		else:
-			print ("button off")
-			GPIO.output(ledPin, GPIO.LOW)
-
-def buzzer_off(dt):
-	GPIO.output(beepPin, GPIO.LOW)
-
-# Toggle the flashing LED according to the speed global
-# This will need better implementation
-def flash(dt):
-	global speed
-	GPIO.output(flashLedPin, not GPIO.input(flashLedPin))
-	Clock.schedule_once(flash, 1.0/speed)
-
-# This is called when the slider is updated:
-def update_speed(obj, value):
-	global speed
-	print("Updating speed to:" + str(obj.value))
-	speed = obj.value
-
-# Modify the Button Class to update according to GPIO input:
-class InputButton(Button):
 	def update(self, dt):
-		if GPIO.input(buttonPin) == True:
+		# work around widget properties not being set
+		# when __init__() is called
+		if not self.gpio_configured:
+			if GPIO.getmode() is None:
+				GPIO.setmode(GPIO.BOARD)
+			print("setting up pin {}".format(self.pin))
+			if self.pull_up_down is not None:
+				GPIO.setup(self.pin, self.direction, pull_up_down=self.pull_up_down)
+			else:
+				GPIO.setup(self.pin, self.direction)
+			if self.gpio_value is not None:
+				GPIO.output(self.pin, self.gpio_value)
+			self.gpio_configured = True
+
+		if GPIO.input(self.pin):
 			self.state = 'normal'
 		else:
-			self.state = 'down'			
+			self.state = 'down'
 
-class MyApp(App):
+""" A button that toggles a GPIO pin when pressed """
+class GPIOToggleButton(GPIOButton, ToggleButton):
+	def on_press(self):
+		super(GPIOToggleButton, self).on_press()
+		GPIO.output(self.pin, not GPIO.input(self.pin))
+		return True
 
+""" A button that alters that state of a GPIO pin when held down """
+class GPIOPressButton(GPIOButton, Button):
+	PWM = None
+	def on_press(self):
+		super(GPIOPressButton, self).on_press()
+		if (self.PWM is None) or (not self.gpio_configured):
+			if GPIO.getmode() is None:
+				GPIO.setmode(GPIO.BOARD)
+			self.PWM = GPIO.PWM(int(self.pin), 3000)
+			self.gpio_configured = True
+		self.PWM.start(25)
+		return True
+
+	def on_release(self):
+		self.PWM.ChangeDutyCycle(0)
+		return True
+
+""" A slider that alters the state of a GPIO pin based on current value """
+class GPIOSlider(Slider):
+	pin = NumericProperty(None)
+	direction = NumericProperty(None)
+	pull_up_down = NumericProperty(None, allownone=True)
+	gpio_configured = BooleanProperty(False)
+	PWM = None
+
+	def on_value(self, instance, value):
+		if (self.PWM is None) or (not self.gpio_configured):
+			# work around widget properties not being set
+			# when __init__() is called
+			if GPIO.getmode() is None:
+				GPIO.setmode(GPIO.BOARD)
+			if self.pin is None:
+				return True
+			print("setting up pin {}".format(self.pin))
+
+			if self.pull_up_down is not None:
+				GPIO.setup(self.pin, self.direction, pull_up_down=self.pull_up_down)
+			else:
+				GPIO.setup(self.pin, self.direction)
+			self.PWM = GPIO.PWM(int(self.pin), int(self.value))
+			self.PWM.start(50)
+			self.gpio_configured = True
+
+		self.PWM.ChangeFrequency(int(self.value))
+		return True
+
+class Monitor(Widget):
+	pass
+
+class MonitorApp(App):
 	def build(self):
-		# Set up the layout:
-		layout = GridLayout(cols=5, spacing=30, padding=30, row_default_height=150)
-
-		# Make the background gray:
-		with layout.canvas.before:
-			Color(.2,.2,.2,1)
-			self.rect = Rectangle(size=(800,600), pos=layout.pos)
-
-		# Instantiate the first UI object (the GPIO input indicator):
-		inputDisplay = InputButton(text="Input")
-
-		# Schedule the update of the state of the GPIO input button:
-		Clock.schedule_interval(inputDisplay.update, 1.0/10.0)
-
-		# Create the rest of the UI objects (and bind them to callbacks, if necessary):
-		outputControl = ToggleButton(text="LED")
-		outputControl.bind(on_press=press_callback)
-		beepButton = Button(text="BEEP!")
-		beepButton.bind(on_press=press_callback)
-		wimg = Image(source='logo.png')
-		speedSlider = Slider(orientation='vertical', min=1, max=30, value=speed)
-		speedSlider.bind(on_touch_down=update_speed, on_touch_move=update_speed)
-
-		# Add the UI elements to the layout:
-		layout.add_widget(wimg)
-		layout.add_widget(inputDisplay)
-		layout.add_widget(outputControl)
-		layout.add_widget(beepButton)
-		layout.add_widget(speedSlider)
-
-		# Start flashing the LED
-		Clock.schedule_once(flash, 1.0/speed)
-
-		return layout
+		return Monitor()
 
 if __name__ == '__main__':
-	MyApp().run()
+	GPIO.setmode(GPIO.BOARD)
+	MonitorApp().run()
+	GPIO.cleanup()
